59769e1cb492863d185706c43bb6b8f7f5ea5b87
zznidar <zznidar@users.noreply.github.com>
2026-05-27 12:49:06 +0200

    Variablise scripts, update readme

--- README.md
+++ README.md
@@ -36,3 +36,23 @@ SPEDEIQALEEENAQLEQENAALEEEIAQLEY
 >P1__P2Args
 SPEDEIQALEEENAQLEQENAALEEEIAQLEY
 ```
+
+# Prosculpt-slurm-runner
+## Installation
+1. Install [Prosculpt](https://github.com/ajasja/prosculpt#installation) 
+2. Clone the ps2slurm folder from this repo
+3. Modify `ps2slurm.config` to use correct paths.
+    * `env_setup_script_path`: script to activate conda environments, modify as needed
+    * Make sure you use full absolute paths
+4. Update the second line of `counter.sh` with the correct config path
+5. `crontab -e`
+6. Esc -> `i` (to start insert mode in Vim)
+7. Paste the content of `cron.sh`, make sure you add a new line
+8. Esc -> `:x` to save  
+    * Should print `crontab: installing new crontab`
+
+The script will now be run every minute. If projects are found in the input directory, it will submit them.
+
+## Usage
+Add your project folder to the input directory. It should contain all needed files (run.yaml, potentially input.pdb, alignment.a3m ...). It will be moved to the output directory and submitted.  
+**Destination folder will be overwritten!** The server is meant to be temporary, so people should copy stuff off.
--- ps2slurm/counter.sh
+++ ps2slurm/counter.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+source /home/d12/03_PROSCULPT/ps2slurm/ps2slurm.config
 
 #echo $(date +"%Y-%m-%dT%H:%M:%S%z") >> /home/d12/03_PROSCULPT/ps2slurm/ura.tece
 
-num=$(find /home/d12/PROSCULPT_MOUNT/in/* -maxdepth 0 -type d -printf x | wc -c)
+num=$(find ${in_folder}/* -maxdepth 0 -type d -printf x | wc -c)
 if [ $num -gt 0 ]; then
-    echo "$(date +"%Y-%m-%dT%H:%M:%S%z") There are $num projects in the directory." >> /home/d12/PROSCULPT_MOUNT/watcher.out.log
-    source /home/d12/03_PROSCULPT/ps2slurm/set_up_prosculpt_env.sh
-    source /home/d12/03_PROSCULPT/ps2slurm/ps2slurm.config2
+    echo "$(date +"%Y-%m-%dT%H:%M:%S%z") There are $num projects in the directory." >> $log_path_name
+    source $env_setup_script_path
     
     # Copy each projectDirectory to the output directory with a unique name
-    for dir in /home/d12/PROSCULPT_MOUNT/in/*; do
+    for dir in ${in_folder}/*; do
         if [ -d "$dir" ]; then
             #timestamp=$(date +"%Y-%m-%dT%H-%M-%S")
             DIRNAME=$(basename "$dir")
@@ -21,10 +21,10 @@ if [ $num -gt 0 ]; then
             # Find the .yaml file in the new folder
             yaml_file=$(find . -maxdepth 1 -type f -iname '*.yaml')
             if [ -f "$yaml_file" ]; then
-                echo "$(date +"%Y-%m-%dT%H:%M:%S%z") Submitting $yaml_file in $NEWFOLDER." >> /home/d12/PROSCULPT_MOUNT/watcher.out.log
+                echo "$(date +"%Y-%m-%dT%H:%M:%S%z") Submitting $yaml_file in $NEWFOLDER." >> $log_path_name
                 python $launch_script_path "$NEWFOLDER/$yaml_file" ++output_dir="$NEWFOLDER"
             else
-                echo "$(date +"%Y-%m-%dT%H:%M:%S%z") [FATAL] No .yaml file found in $NEWFOLDER." >> /home/d12/PROSCULPT_MOUNT/watcher.out.log
+                echo "$(date +"%Y-%m-%dT%H:%M:%S%z") [FATAL] No .yaml file found in $NEWFOLDER." >> $log_path_name
             fi
         fi
     done
