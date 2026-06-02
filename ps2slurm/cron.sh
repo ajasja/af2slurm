0725b6ed3934a7d05ce1b8dc74e65faadf38b00e
zznidar <zznidar@users.noreply.github.com>
2026-06-02 11:56:22 +0200

    Rename counter to watcher

--- ps2slurm/cron.sh
+++ ps2slurm/cron.sh
@@ -1,3 +1,3 @@
-# Make sure the counter.sh file has execute permissions: `chmod +x counter.sh`
+# Make sure the watcher.sh file has execute permissions: `chmod +x watcher.sh`
 # Copy and modify this file into `crontab -e`. Ensure there's a newline at the end.
-* * * * * /home/d12/03_PROSCULPT/ps2slurm/counter.sh
+* * * * * /home/d12/03_PROSCULPT/ps2slurm/watcher.sh
